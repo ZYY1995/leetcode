904f68e6e05719656c9f11ada42a69fb0f0b598c
ZYY1995 <793076505ZYY@gmail.com>
2017-10-17 01:49:24 +0800

    problem 1-3 submit

--- 1.Two Sum.py
+++ 1.Two Sum.py
@@ -7,9 +7,7 @@ class Solution(object):
         """
         dic = {}
         for i in range(0,len(nums)): 
-			dic[nums[i]] = i
-            
-		for i in range(0,len(nums)): 
 			tmp = target - nums[i]
             if dic.has_key(tmp) and not dic[tmp] == i:
                 return [i,dic[tmp]]
+			dic[nums[i]] = i
