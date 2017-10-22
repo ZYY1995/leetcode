446d57c41d77845b5b4f76449be061061bff06f7
ZYY1995 <793076505ZYY@gmail.com>
2017-10-22 17:34:46 +0800

    problem 561-2 submit

--- 561. Array Partition I.cpp
+++ 561. Array Partition I.cpp
@@ -2,11 +2,36 @@ class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
         int sum = 0;
-        sort(nums.begin(), nums.end());
-        for (int i = 0;i < nums.size();i = i + 2)
+        vector<int> hashtable(20001,0);
+        for(int i = 0;i<nums.size();i++)
         {
-            sum += nums[i];
+            hashtable[nums[i] + 10000]++;
+        }
+        bool flag = true;
+        for(int i = 0;i<20001;)
+        {
+            if (hashtable[i])
+            {
+                if (flag)
+                {
+                    sum += i - 10000;
+                    flag = false;
+                    
+                }
+                else
+                {
+                    flag = true;
+                }
+                hashtable[i]--;
+            }
+            else
+            {
+                i++;
+            }
         }
         return sum;
     }
 };
+/*
+存在[1,2,3,2]的情况，因此每次必须hashtable[i]--直到hashtable[i] == 0才能i++
+*/
