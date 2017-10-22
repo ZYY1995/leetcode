e98e83a52b1ca865c05964e8cf17ad1427bf6ace
ZYY1995 <793076505ZYY@gmail.com>
2017-10-23 00:11:02 +0800

    problem 412-1 submit

---
+++ 412. Fizz Buzz.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+        int fizz = 0,buzz = 0;
+        for(int i = 1; i <= n;i++)
+        {
+            fizz++;
+            buzz++;
+            if(fizz == 3 && buzz == 5)
+            {
+                result.push_back("FizzBuzz");
+                fizz = 0;
+                buzz = 0;
+                continue;
+            }
+            if(fizz == 3)
+            {
+                result.push_back("Fizz");
+                fizz = 0;
+                continue;
+            }
+            if(buzz == 5)
+            {
+                result.push_back("Buzz");
+                buzz = 0;
+                continue;
+            }
+            result.push_back(to_string(i));
+        }
+        return result;   
+    }
+};
