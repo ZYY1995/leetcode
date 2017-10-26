7f899a517625812927479574132c81f6892c3940
ZYY1995 <793076505ZYY@gmail.com>
2017-10-27 01:01:39 +0800

    problem 136-1 submit,good solution XOR

--- 136. Single Number.cpp
+++ 136. Single Number.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int> a;
+        int result = 0;
         for(int i = 0;i < nums.size();i++)
         {
-            a[nums[i]] += 1;
-            if (a[nums[i]] > 1)
-            {
-                a.erase(nums[i]);
-            }
+            result ^= nums[i];
         }
-        return a.begin()->first;
+        return result;
     }
 };
