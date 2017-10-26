e2460d797794e056d48be849f821f4bacf07ca09
ZYY1995 <793076505ZYY@gmail.com>
2017-10-27 00:12:24 +0800

    problem 637-1 submit,BFS

--- 637. Average of Levels in Binary Tree.cpp
+++ 637. Average of Levels in Binary Tree.cpp
@@ -7,41 +7,26 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-struct Result{
-    vector<double> value;
-    vector<int> count;
-};
 class Solution {
 public:
-    Result countOfLevels(TreeNode* root) {
-        if (root == NULL)
+    vector<double> averageOfLevels(TreeNode* root) {
+        queue<TreeNode*> q;
+        vector<double> result;
+        q.push(root);
+        while(!q.empty())
         {
-            Result result;
-            return result;
-        }
-            
-        Result result;
-        result.count.push_back(1);
-        result.value.push_back(root->val);
-        
-        Result left = countOfLevels(root->left);
-        Result right = countOfLevels(root->right);
-        int i = 0;
-        while(i < left.value.size() || i < right.value.size())
-        {
-            result.value.push_back((i<left.value.size()?left.value[i]:0) + (i<right.value.size()?right.value[i]:0));
-            result.count.push_back((i<left.value.size()?left.count[i]:0) + (i<right.value.size()?right.count[i]:0));
-            i++;
+            double tmp = 0;
+            int n_size = q.size();
+            for(int i = 0;i < n_size;i++)
+            {
+                TreeNode* t = q.front();
+                q.pop();
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+                tmp += t->val;
+            }
+            result.push_back(tmp / n_size);
         }
         return result;
-    }
-    
-    vector<double> averageOfLevels(TreeNode* root) {
-        Result result = countOfLevels(root);
-        for(int i = 0;i<result.value.size();i++)
-        {
-            result.value[i] /= result.count[i];
-        }
-        return result.value;
     }
 };
