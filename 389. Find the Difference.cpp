7d290e0b5b3c4ea018316fcd83b160402ad9b753
ZYY1995 <793076505ZYY@gmail.com>
2017-11-22 01:16:09 +0800

    problem 389-2 submit.clean code,use bit

--- 389. Find the Difference.cpp
+++ 389. Find the Difference.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int num_letter[26] = {0};
-        for(int i = 0;i < s.size();i++)
+        char c = t[t.size() - 1];
+        for(int i = 0 ;i < s.size();i++)
         {
-            num_letter[s[i] -  97]++;
+            c ^= s[i];
+            c ^= t[i];
         }
-        for(int i = 0;i<t.size();i++)
-        {
-            if(num_letter[t[i] - 97] == 0)
-            {
-                return t[i];
-            }
-            num_letter[t[i] - 97]--;
-        }
+        return c;
     }
 };
